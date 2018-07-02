b0994df7d1e745d0b124764285b978b63ed13686
whyCaiji <wuheyang0617@gmail.com>
2018-07-03 01:41:58 +0800

    财务本地化相关脚本和元数据的初次提交

---
+++ ncscript/SQLSERVER/新加坡_总账_科目辅助余额表支持辅助核算分列显示及打印及上级科目显示编码.sql
@@ -0,0 +1,4 @@
+delete from pub_print_dataitem where pk_varitem = '2002Z303000000770001';
+
+insert into pub_print_dataitem (DR, IDATATYPE, ITYPE, PK_CORP, PK_VARITEM, PREPARE1, RESID, TS, USERDEFFLAG, VNODECODE, VTABLECODE, VTABLENAME, VVAREXPRESS, VVARNAME)
+values (0, null, null, '@@@@', '2002Z303000000770001', null, '', '2014-12-03 20:15:36', null, '20028003', null, null, 'assid', N'¸¨ÖúºËËãÖ÷¼ü');
